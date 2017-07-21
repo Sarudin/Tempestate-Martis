0b55d2b9a4b9ae5f101959cad039c2695bc0a297
Sarudin <sarudin1wow@gmail.com>
2017-07-20 21:06:14 -0600

    Double crap

--- js/weatherService.js
+++ js/weatherService.js
@@ -1,8 +1,8 @@
 angular.module('app').service('weatherService', function($http) {
   this.getAll = () => {
     return $http.get('/api/getAll').then(response => {
-      console.log("SHIT!!!");
-      return response;
+      console.log("response in service = " + response.data);
+      return response.data;
     })
   }
 })
